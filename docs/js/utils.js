e785b895662eec1c1ac845433de8d26f82d1dffe
apoyomedicoips <apoyomedicoips@gmail.com>
2025-12-08 04:45:52 -0300

    Actualización dashboard IPS 2025

--- docs/js/utils.js
+++ docs/js/utils.js
@@ -1,38 +1,11 @@
-// utils.js
+
 const Utils = {
-    formatNumber: (num) => {
-        if (num === null || num === undefined) return "0";
-        return new Intl.NumberFormat('es-PY').format(num);
-    },
-
-    formatPercentage: (num) => {
-        if (num === null || num === undefined) return "0.0%";
-        return `${(num * 100).toFixed(1)}%`;
-    },
-
+    formatNumber: (num) => new Intl.NumberFormat('es-PY').format(num || 0),
+    formatPercentage: (num) => num ? num.toFixed(1) + '%' : '0%',
     formatDate: (dateString) => {
         if (!dateString) return "-";
         const date = new Date(dateString);
-        return date.toLocaleDateString('es-PY', {
-            year: 'numeric',
-            month: 'long',
-            day: 'numeric',
-            hour: '2-digit',
-            minute: '2-digit'
-        });
-    },
-
-    debounce: (func, wait) => {
-        let timeout;
-        return function executedFunction(...args) {
-            const later = () => {
-                clearTimeout(timeout);
-                func(...args);
-            };
-            clearTimeout(timeout);
-            timeout = setTimeout(later, wait);
-        };
+        return date.toLocaleDateString('es-PY', { year: 'numeric', month: 'long', day: 'numeric' });
     }
 };
-
 window.Utils = Utils;
